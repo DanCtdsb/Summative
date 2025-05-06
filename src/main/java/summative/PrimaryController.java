9f4779b162fdb0e3b5b395c16bbdc5c888f5b7dd
unknown <343025052@TDSB.ON.CA>
2025-05-06 11:38:12 -0400

    Added filters, transformation, etc

---
+++ src/main/java/summative/PrimaryController.java
@@ -0,0 +1,230 @@
+package summative;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.IOException;
+import javafx.embed.swing.SwingFXUtils;
+import javax.imageio.ImageIO;
+import javafx.scene.paint.Color;
+
+public class PrimaryController {
+
+    private Stage stage;
+    private Image originalImage; // Use this to keep track of the original image
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private MenuItem openImage;
+
+    @FXML
+    private MenuItem saveImage;
+
+    @FXML
+    private MenuItem horizontalFlip;
+
+    @FXML
+    private MenuItem verticalFlip;
+
+    @FXML
+    private MenuItem counterClockwiseRotation;
+
+    @FXML
+    private MenuItem clockwiseRotation;
+
+    @FXML
+    private MenuItem invert;
+
+    @FXML
+    private MenuItem greyscale;
+
+    @FXML
+    private MenuItem reset;
+
+    @FXML
+    void onOpenImage(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image File");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif"));
+
+        try {
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                Image image = new Image(file.toURI().toString());
+                originalImage = image;
+                imageView.setImage(image);
+            }
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Image Load Failed");
+            alert.setContentText("There was a problem opening your image");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    public void onSaveImage(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Image");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG files", "*.png"));
+        File file = fileChooser.showSaveDialog(imageView.getScene().getWindow());
+
+        if (file != null) {
+            WritableImage writableImage = imageView.snapshot(null, null);
+            try {
+                ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "png", file);
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Image Save Failed");
+                alert.setContentText("There was a problem saving your image");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    @FXML
+    void onHorizontalFlip(ActionEvent event) {
+        int width = (int) imageView.getImage().getWidth();
+        int height = (int) imageView.getImage().getHeight();
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelReader reader = imageView.getImage().getPixelReader();
+        PixelWriter writer = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                writer.setColor(width - i - 1, j, reader.getColor(i, j));
+            }
+        }
+        imageView.setImage(writableImage);
+    }
+
+    @FXML
+    void onVerticalFlip(ActionEvent event) {
+        int width = (int) imageView.getImage().getWidth();
+        int height = (int) imageView.getImage().getHeight();
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelReader reader = imageView.getImage().getPixelReader();
+        PixelWriter writer = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                writer.setColor(width - i - 1, height - j - 1, reader.getColor(i, j));
+            }
+        }
+        imageView.setImage(writableImage);
+    }
+
+    @FXML
+    void onCounterClockwiseRotation(ActionEvent event) {
+        int width = (int) imageView.getImage().getWidth();
+        int height = (int) imageView.getImage().getHeight();
+
+        WritableImage writableImage = new WritableImage(height, width);
+        PixelReader reader = imageView.getImage().getPixelReader();
+        PixelWriter writer = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                writer.setColor(j, width - i - 1, reader.getColor(i, j));
+            }
+        }
+        imageView.setImage(writableImage);
+    }
+    @FXML
+    void onClockwiseRotation(ActionEvent event) {
+        int width = (int) imageView.getImage().getWidth();
+        int height = (int) imageView.getImage().getHeight();
+
+        WritableImage writableImage = new WritableImage(height, width);
+        PixelReader reader = imageView.getImage().getPixelReader();
+        PixelWriter writer = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                writer.setColor(height - j - 1, i, reader.getColor(i, j));
+            }
+        }
+        imageView.setImage(writableImage);
+    }
+
+    @FXML
+    void onInvert(ActionEvent event) {
+        int width = (int) imageView.getImage().getWidth();
+        int height = (int) imageView.getImage().getHeight();
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelReader reader = imageView.getImage().getPixelReader();
+        PixelWriter writer = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Color color = reader.getColor(i, j);
+                Color newColor = new Color(1.0 - color.getRed(), 1.0 - color.getGreen(), 1.0 - color.getBlue(), 1);
+                writer.setColor(i, j, newColor);
+            }
+        }
+        imageView.setImage(writableImage);
+    }
+
+    @FXML
+    void onGrayscale(ActionEvent event) {
+        int width = (int) imageView.getImage().getWidth();
+        int height = (int) imageView.getImage().getHeight();
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelReader reader = imageView.getImage().getPixelReader();
+        PixelWriter writer = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Color color = reader.getColor(i, j);
+                double shade = (color.getRed() + color.getGreen() + color.getBlue()) / 3;
+                Color newColor = new Color(shade, shade, shade, 1);
+                writer.setColor(i, j, newColor);
+            }
+        }
+        imageView.setImage(writableImage);
+    }
+
+    @FXML
+    void onReset(ActionEvent event) {
+        imageView.setImage(originalImage);
+    }
+
+    /*
+     * Accessing a pixels colors
+     * 
+     * Color color = reader.getColor(x, y);
+     * double red = color.getRed();
+     * double green = color.getGreen();
+     * double blue = color.getBlue();
+     */
+
+    /*
+     * Modifying a pixels colors
+     * 
+     * Color newColor = new Color(1.0 - red, 1.0 - green, 1.0 - blue,
+     * color.getOpacity());
+     */
+
+    // DO NOT REMOVE THIS METHOD!
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
